787ca2a9689da6b4d1bff860be905e693bd8e066
J.Fernando Sánchez <balkian@gmail.com>
2012-05-11 13:41:48 +0200

    Corrected play kiss

--- play-kiss.sh
+++ play-kiss.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rtmpdump -r rtmp://kissfm.es.flash3.glb.ipercast.net/kissfm.es-live --playpath aac.stream | mplayer -vo null -idle -
-
+rtmpdump --live --quiet -r rtmp://kissfm.es.flash3.glb.ipercast.net/kissfm.es-live --playpath aac.stream | mplayer -vo null -idle -
