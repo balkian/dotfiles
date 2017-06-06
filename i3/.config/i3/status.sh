6a2f0dec9daade9e6bca2c754aba20709dc5b8cc
J. Fernando Sánchez <balkian@gmail.com>
2017-06-06 13:25:32 +0200

    Fixed i3 status

--- i3/.config/i3/status.sh
+++ i3/.config/i3/status.sh
@@ -1,2 +1,6 @@
 #1/bin/sh
-SCRIPT_DIR=/usr/lib/i3blocks i3blocks -c $HOME/.config/i3/i3blocks.conf
+SCRIPT_DIR=/usr/lib/i3blocks
+if [ ! -d "$SCRIPT_DIR" ]; then
+  SCRIPT_DIR=/usr/share/i3blocks
+fi
+SCRIPT_DIR=$SCRIPT_DIR i3blocks -c $HOME/.config/i3/i3blocks.conf
