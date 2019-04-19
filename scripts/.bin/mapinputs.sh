6cb60e7a27855163b7f62d4ef34d79a23f69f361
J. Fernando Sánchez <balkian@gmail.com>
2019-04-19 21:14:38 +0200

    gopher changes

--- scripts/.bin/mapinputs.sh
+++ scripts/.bin/mapinputs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xinput map-to-output 16 eDP-1 # Eraser
-xinput map-to-output 12 eDP-1 # Stylus
-xinput map-to-output 11 eDP-1 # Touch
+xinput map-to-output "ELAN9038:00 04F3:261A Pen eraser" eDP-1 # Eraser
+xinput map-to-output "ELAN9038:00 04F3:261A Pen stylus" eDP-1 # Stylus
+xinput map-to-output "ELAN9038:00 04F3:261A Touchscreen" eDP-1 # Touch
