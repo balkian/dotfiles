2574a624033c6cd3164949fe7913bf9caa617f68
J. Fernando Sánchez <balkian@gmail.com>
2020-04-15 17:19:21 +0200

    Batch of changes

--- screenlayouts/.screenlayout/single.sh
+++ screenlayouts/.screenlayout/single.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI1 --off --output DVI-1 --off --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output VGA1 --off
+xrandr --output HDMI1 --off --output DVI-1 --off --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output VGA1 --off --auto
