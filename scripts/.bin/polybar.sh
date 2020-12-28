d1067cbb0030a339bcfce855c93383dc9a882227
J. Fernando Sánchez <balkian@gmail.com>
2020-12-28 14:58:13 +0100

    Fix polybar.sh
    After the last update, I started getting duplicated bars when using cloned
    monitors.
    
    This commit changes the polybar.sh script to use replace xrandr with a call to
    `polybar --list-monitors`, which ignores cloned monitors by default.

--- scripts/.bin/polybar.sh
+++ scripts/.bin/polybar.sh
@@ -6,10 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload main &
-    done
-else
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload main &
+done
     polybar --reload main &
 fi
