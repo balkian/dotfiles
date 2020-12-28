f395ed8f03eb9c87070302a4eabf2feac4769a7f
J. Fernando Sánchez <balkian@gmail.com>
2020-12-28 16:21:25 +0100

    Add rofi themes and bluetooth menu
    + Fixed dunst

--- scripts/.bin/winmenu.sh
+++ scripts/.bin/winmenu.sh
@@ -3,7 +3,7 @@
 
 if command -v rofi >/dev/null 2>&1;
 then
-    program="rofi -show window"
+    program="rofi -show window -combi-hide-mode-prefix -combi-modi drun,run -theme .bin/rofi-slate-theme.rasi"
 else
     program="i3-winmenu.py"
 fi
