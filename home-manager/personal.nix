cf50f8800b477f5490ca7a7136f72d5364befd82
J. Fernando Sánchez <balkian@gmail.com>
2026-02-05 10:44:04 +0100

    Home manager: Remove unnecessary git config

--- home-manager/personal.nix
+++ home-manager/personal.nix
@@ -1,13 +1,4 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [ ];
-  programs.git = {
-      enable = true;
-      userName = "J. Fernando Sánchez";
-      userEmail = "balkian@gmail.com";
-      extraConfig = {
-              init.defaultBranch = "main";
-      };
-  };
-
 }
