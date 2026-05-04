0e71c1a63792de19ca1d742ad4a8a9db244735f7
J. Fernando Sánchez <j@sanchezrada.es>
2026-05-04 10:13:10 +0200

    Add vscode config

--- home-manager/lenny.nix
+++ home-manager/lenny.nix
@@ -1,16 +1,42 @@
 { config, pkgs, ... }:
 {
+  nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
-	ansible
-	awscli2
-	cargo-cross
-	rustup
-	rustc
-	gcc
-	python3Packages.jedi-language-server
-	ruff
-	vial
-	chromium
+    ansible
+    awscli2
+    cargo-cross
+    rustup
+    rustc
+    gcc
+    python3Packages.jedi-language-server
+    ruff
+    vial
+    chromium
+
+    # Bloque VS Code corregido
+    ((pkgs.vscode-with-extensions.override {
+      vscodeExtensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        ms-python.python
+        ms-vscode-remote.remote-containers
+        ms-azuretools.vscode-docker
+        ms-vscode-remote.remote-ssh
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+      ];
+    }).overrideAttrs (oldAttrs: {
+      makeWrapperArgs = (oldAttrs.makeWrapperArgs or []) ++ [
+        "--set-default" "NIXOS_OZONE_WL" "1" 
+      ];
+    })) # Los paréntesis dobles aseguran que se evalúe como un paquete único
+
   ];
 
+  home.file = {
+    ".config/Code/User/settings.json".text = ''
+    {
+        "window.titleBarStyle": "custom"
+    }
+    '';
+  };
 }
